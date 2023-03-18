4dc10fc966298c879e1b378d6c6235aefaf78d79
Soumyajagupta07 <guptasoumyaja@gmail.com>
2023-03-18 12:12:06 -0700

    committed

--- ToDoList/index.js
+++ ToDoList/index.js
@@ -3,21 +3,32 @@ let task_input = document.getElementById('task_input');
 let p = document.getElementById('para');
 
 b.addEventListener('click',() => {
-    let list = document.createElement('li');
-    list.innerHTML = task_input.value;
-    let remove_task = document.createElement('button');
-    remove_task.innerHTML = 'Remove';
-    list.appendChild(remove_task);
-    remove_task.style.marginLeft='20px'
-    p.appendChild(list)
+    let li = document.createElement('li');
+    li.innerHTML = task_input.value;
+    // let remove_task = document.createElement('button');
+    // remove_task.innerHTML = 'Remove';
+    // list.appendChild(remove_task);
+    // remove_task.style.marginLeft='20px'
+    p.appendChild(li)
     task_input.value = null;
-    // list.style.listStyle = 'disc inside';
+    li.setAttribute('class' ,'list')
+    li.style.listStyle = 'disc inside';
+    let button_click = document.querySelectorAll('.list')
+    for(let i of button_click){
+        i.addEventListener('click',(e)=>{
+            if(e.target.classList.contains('list')){
+                i.remove(e)
+                // i.remove(li)
+            }
+        })
+
+    }
 })
 
 remove_b.addEventListener('click',()=>{
     p.innerHTML = null;
 })
 
-remove_task.addEventListener('click',()=>{
-    list.innerHTML = null;
-})
+// remove_task.addEventListener('click',()=>{
+//     list.innerHTML = null;
+// })
